bdeef9e0e6b77fa7db0e6183fc40a9ff6b90992f
Jack Choi <648074155@qq.com>
2021-05-14 09:55:42 +0800

    Update linked_hash_map.rs

--- src/linked_hash_map.rs
+++ src/linked_hash_map.rs
@@ -1,1 +1,17 @@
+use std::borrow::Borrow;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash,Hasher};
+const INITIAL_NBUCKETS:usize=1;
 
+pub struct HashMap<K,V>{
+    buckets:Vec<Vec<(K,V)>>,
+    item:usize,
+}
+impl<K,V> HashMap<K,V>{
+    pub fn new()->Self{
+        HashMap{
+            buckets:Vec::new(),
+            item:0,
+        }
+    }
+}
