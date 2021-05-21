f3861993f81fda8b21ec19149d94176c8345d0f4
Jack Choi <648074155@qq.com>
2021-05-21 14:45:13 +0800

    Update linked_hash_map.rs

--- src/linked_hash_map.rs
+++ src/linked_hash_map.rs
@@ -15,4 +15,25 @@ impl<K,V> HashMap<K,V>{
         }
     }
 }
+pub struct OccupiedEntry<'a, K: 'a, V: 'a> {
+    entry: &'a mut (K, V),
+}
+
+pub struct VacantEntry<'a, K: 'a, V: 'a> {
+    key: K,
+    map: &'a mut HashMap<K, V>,
+    bucket: usize,
+}
+
+impl<'a, K: 'a, V: 'a> VacantEntry<'a, K, V> {
+    pub fn insert(self, value: V) -> &'a mut V
+    where
+        K: Hash + Eq,
+    {
+        self.map.buckets[self.bucket].push((self.key, value));
+        self.map.items += 1;
+        &mut self.map.buckets[self.bucket].last_mut().unwrap().1
+    }
+}
+
 //TODO
