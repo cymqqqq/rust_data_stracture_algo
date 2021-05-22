b98bb72469152b81573c1c9a104938f4cce2803c
Jack Choi <648074155@qq.com>
2021-05-22 15:14:31 +0800

    Update complex.rs

--- src/complex.rs
+++ src/complex.rs
@@ -38,3 +38,58 @@ impl Add<Complex<f64>> for f64 {
         Complex { re: rhs.re + self, im: rhs.im }
     }
 }
+use std::ops::Neg;
+
+impl<T, O> Neg for Complex<T>
+    where T: Neg<Output=O>
+{
+    type Output = Complex<O>;
+    fn neg(self) -> Self::Output {
+        Complex { re: -self.re, im: -self.im }
+    }
+}
+use std::ops::AddAssign;
+
+impl<T> AddAssign for Complex<T>
+    where T: AddAssign<T>
+{
+    fn add_assign(&mut self, rhs: Complex<T>) {
+        self.re += rhs.re;
+        self.im += rhs.im;
+    }
+}
+impl<T: PartialEq> PartialEq for Complex<T> {
+    fn eq(&self, other: &Complex<T>) -> bool {
+        self.re == other.re && self.im == other.im
+    }
+}
+
+impl<T: Eq> Eq for Complex<T> { }
+use std::fmt;
+
+// To make the formatting examples mesh with the rest of this file, I've adapted
+// them to work on the type `Complex<f64>`, where the book simply defines a new
+// non-generic `Complex` type. The only changes are adding `<f64>`, and changing
+// the field names.
+
+#[cfg(skip)]
+impl fmt::Display for Complex<f64> {
+    fn fmt(&self, dest: &mut fmt::Formatter) -> fmt::Result {
+        let i_sign = if self.i < 0.0 { '-' } else { '+' };
+        write!(dest, "{} {} {}i", self.r, i_sign, f64::abs(self.i))
+    }
+}
+
+impl fmt::Display for Complex<f64> {
+    fn fmt(&self, dest: &mut fmt::Formatter) -> fmt::Result {
+        let (r, i) = (self.re, self.im);
+        if dest.alternate() {
+            let abs = f64::sqrt(r * r + i * i);
+            let angle = f64::atan2(i, r) / std::f64::consts::PI * 180.0;
+            write!(dest, "{} ∠ {}°", abs, angle)
+        } else {
+            let i_sign = if i < 0.0 { '-' } else { '+' };
+            write!(dest, "{} {} {}i", r, i_sign, f64::abs(i))
+        }
+    }
+}
