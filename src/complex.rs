ffba0b57e103086785c1f88e8d3dfe5f022b6978
Jack Choi <648074155@qq.com>
2021-05-22 10:54:46 +0800

    Update complex.rs

--- src/complex.rs
+++ src/complex.rs
@@ -1,1 +1,24 @@
+use std::ops::Add;
+use std::ops::Mul;
+struct Complex<T> {
+    /// Real portion of the complex number
+    re: T,
 
+    /// Imaginary portion of the complex number
+    im: T
+}
+#[cfg(skip)]
+impl Add for Complex<i32> {
+    type Output = Complex<i32>;
+    fn add(self, rhs: Self) -> Self {
+        Complex { re: self.re + rhs.re, im: self.im + rhs.im }
+    }
+}
+impl<T> Add for Complex<T>
+    where T: Add<Output=T>
+{
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        Complex { re: self.re + rhs.re, im: self.im + rhs.im }
+    }
+}
