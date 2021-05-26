153579e8c17597d3ca6cd442c6c3b1e7cce69137
Jack Choi <648074155@qq.com>
2021-05-26 22:30:29 +0800

    Update middle_of_link_list.rs

--- src/middle_of_link_list.rs
+++ src/middle_of_link_list.rs
@@ -27,16 +27,7 @@ macro_rules! linked{
     ($($e:expr),*)=>{to_list(vec![$($e.to_owned()),*])};
     ($($e:expr),*)=>{to_list(vec![$($e.to_owned()),*])};
 }
-pub fn has_cycle(head:Option<Box<ListNode>>)->bool{
-    let mut fast_p=&head;
-    let mut slow_p=&head;
-    while fast_p.is_some() && fast_p.as_ref().unwrap().next.is_some(){
-        slow_p=&slow_p.as_ref().unwrap().next;
-        fast_p=&fast_p.as_ref().unwrap().next.as_ref().unwrap().next;
-        if slow_p==fast_p {return true;}
-    }
-    false
-}
+
 
 pub fn middle_node(head:Option<Box<ListNode>>)->Option<Box<ListNode>>{
     let mut fast_p=&head;
