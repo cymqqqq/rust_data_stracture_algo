24edcc87ae3d0ba9d5962cb974a4638aab072862
Jack Choi <648074155@qq.com>
2021-05-26 22:30:09 +0800

    Create middle_of_link_list.rs

---
+++ src/middle_of_link_list.rs
@@ -0,0 +1,53 @@
+//defintion for singly-linked list
+#[derive(PartialEq,Eq,Clone,Debug)]
+pub struct ListNode{
+    pub val:i32,
+    pub next:Option<Box<ListNode>>
+}
+impl ListNode{
+    #[inline]
+    fn new(val:i32)->Self{
+        ListNode{
+            next:None,
+            val
+        }
+    }
+}
+pub fn to_list(vec:Vec<i32>)->Option<Box<ListNode>>{
+    let mut current=None;
+    for &v in vec.iter().rev(){
+        let mut node=ListNode::new(v);
+        node.next=current;
+        current=Some(Box::new(node));
+    }
+    current
+}
+#[macro_export]
+macro_rules! linked{
+    ($($e:expr),*)=>{to_list(vec![$($e.to_owned()),*])};
+    ($($e:expr),*)=>{to_list(vec![$($e.to_owned()),*])};
+}
+pub fn has_cycle(head:Option<Box<ListNode>>)->bool{
+    let mut fast_p=&head;
+    let mut slow_p=&head;
+    while fast_p.is_some() && fast_p.as_ref().unwrap().next.is_some(){
+        slow_p=&slow_p.as_ref().unwrap().next;
+        fast_p=&fast_p.as_ref().unwrap().next.as_ref().unwrap().next;
+        if slow_p==fast_p {return true;}
+    }
+    false
+}
+
+pub fn middle_node(head:Option<Box<ListNode>>)->Option<Box<ListNode>>{
+    let mut fast_p=&head;
+    let mut slow_p=&head;
+    while fast_p.is_some()&& fast_p.as_ref().unwrap().next.is_some(){
+        slow_p=&slow_p.as_ref().unwrap().next;
+        fast_p=&fast_p.as_ref().unwrap().next.as_ref().unwrap().next;
+    }
+    slow_p.clone()
+}
+fn main(){
+    println!("{:?}", middle_node(to_list(vec![1, 3, 4])));
+
+}
