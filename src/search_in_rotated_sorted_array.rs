ff16ffcb4ed4f68daa7037a5b7b66a6d1d456353
Jack Choi <648074155@qq.com>
2021-05-28 09:26:05 +0800

    Create search_in_rotated_sorted_array.rs

---
+++ src/search_in_rotated_sorted_array.rs
@@ -0,0 +1,31 @@
+pub fn search(nums:Vec<i32>,target:i32)->i32{
+    if nums.is_empty(){return -1;}
+    let mut low=0;
+    let mut high=nums.len()-1;
+    while low<=high{
+        let mid=low+((high-low)>>1);
+        if nums[mid]==target { return mid as i32;}
+        //left is order
+        if nums[low]<=nums[mid]{
+            if nums[low]<=target && target <= nums[mid]{
+                high=mid-1;
+            }else{
+                low=mid+1;
+                
+            }
+        }else{
+            if nums[mid] <= target && target <= nums[high]{
+                low = mid + 1;
+                
+            }else{
+                high = mid - 1;
+            }
+        }
+    }
+    -1
+}
+fn main() {
+    let nums = vec![4,5,6,7,0,1,2];
+    let n = search(nums, 0);
+    println!("{:?}", n);
+}
